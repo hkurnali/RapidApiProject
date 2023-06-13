80c8c5d9496f91ec5703bb9704ea05d5a1107a3a
hkurnali <huseyinkurnali@hotmail.com>
2023-06-13 22:04:21 +0300

    Add project files.

---
+++ RapidApiProject/Controllers/ImdbController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+
+namespace RapidApiProject.Controllers
+{
+    public class ImdbController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ApiMovieListViewModel> apiMovieListViewModels = new List<ApiMovieListViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "b37b4acdfamsh6f08320c82c5cc1p1ce877jsnd2812b8fceaa" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovieListViewModels = JsonConvert.DeserializeObject<List<ApiMovieListViewModel>>(body);
+                return View(apiMovieListViewModels);
+            }
+            
+        }
+    }
+}
---
+++ RapidApiProject/Models/ApiMovieListViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace RapidApiProject.Models
+{
+    public class ApiMovieListViewModel
+    {
+        
+            public int rank { get; set; }
+            public string title { get; set; }
+           
+            public string rating { get; set; }
+          
+            public string trailer { get; set; }
+            
+       
+    }
+}
+
+
